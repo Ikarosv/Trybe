9578332b4dcd501dd48e76e20557eec1b5aafad7
Ikarosv <ikarovieiraa@gmail.com>
2022-08-18 17:28:37 -0300

    2 mini exercicios adicionado

---
+++ Modulo-1/Bloco-04/1: JavaScript - Primeiros passos/Tipos Primitivos, Tipagem Dinâmica e Operações Aritméticas/script.js
@@ -0,0 +1,19 @@
+// Parte 1
+
+/*
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientEmail)
+*/
+
+// Parte 2
+
+const base = 5;
+let height = 8;
+const area = base * height;
