5c229106818fff4143af18d7bb161bd02b95cc62
Ikarosv <ikarovieiraa@gmail.com>
2022-08-19 11:16:49 -0300

    Exercício do 1 ao 3 feito

---
+++ Modulo-1/Bloco-04/1: JavaScript - Primeiros passos/Execícios/operacoes.js
@@ -0,0 +1,8 @@
+const a = 10;
+const b = 10;
+
+console.log("ADIÇAO: " + (a + b))
+console.log('SUBTRAÇÃO: ' + (a - b))
+console.log('MULTIPLICAÇÃO: ' + (a* b))
+console.log('DIVISAO: ' + (a / b))
+console.log('MÓDULO: ' + (a % b))
