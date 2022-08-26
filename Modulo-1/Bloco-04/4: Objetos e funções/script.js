54a665b97978e9f9254ea94ce3b921fcd5ead95e
Ikarosv <ikarovieiraa@gmail.com>
2022-08-26 17:07:07 -0300

    DEsafio 1 feito

--- Modulo-1/Bloco-04/4: Objetos e funções/script.js
+++ Modulo-1/Bloco-04/4: Objetos e funções/script.js
@@ -1,21 +1,24 @@
 // PRIMEIRA FORMA DE RESOLVER
 
 function numeroQueMaisRepete(arr = [2, 3, 3, 5, 8, 2, 3]) {
-  let numerosEIndices = {}
+  let numerosEIndices = {};
   for (let i = 0; i < arr.length; i += 1) {
-    arr[i] in numerosEIndices ? numerosEIndices[arr[i]].push(i) : numerosEIndices[arr[i]] = [i];
+    arr[i] in numerosEIndices
+      ? numerosEIndices[arr[i]].push(i)
+      : (numerosEIndices[arr[i]] = [i]);
   }
-  let numero = 0
+  let numero = 0;
   for (const key in numerosEIndices) {
-    numero = numero == 0 ? key : numero
+    numero = numero == 0 ? key : numero;
     for (const key2 in numerosEIndices) {
-      numero = numerosEIndices[numero].length < numerosEIndices[key2].length ? key2 : numero
+      numero =
+        numerosEIndices[numero].length < numerosEIndices[key2].length ? key2 : numero;
     }
   }
-  return numero
+  return numero;
 }
 
-console.log(numeroQueMaisRepete())
+console.log(numeroQueMaisRepete());
 
 // SEGUNDA FORMA DE RESOLVER
 
