afd434c90b4ebd2717a7b024a9e8d80dc11bebcf
Ikarosv <ikarovieiraa@gmail.com>
2022-09-22 16:21:13 -0300

    Projeto Unit Tests

---
+++ Modulo-1/Section-07/Projeto Unit Tests/tests/productDetails.spec.js
@@ -0,0 +1,53 @@
+const productDetails = require('../src/productDetails');
+/*
+  A função productDetails recebe duas strings que representam nomes de produtos, e retorna um array contendo dois objetos com os detalhes dos respectivos produtos.
+
+  Parâmetros:
+  - Uma string;
+  - Uma string;
+
+  Comportamento:
+  productDetails('Alcool gel', 'Máscara')
+
+  // Retorna:
+  [
+    {
+      name: 'Alcool gel'
+      details: {
+        productId: 'Alcool gel123'
+      }
+    },
+    {
+      name: 'Máscara'
+      details: {
+        productId: 'Máscara123'
+      }
+    }
+  ]
+
+  Escreva pelo menos cinco testes para essa função para garantir que a implementação de productDetails está correta.
+
+*/
+
+describe('6 - Implemente os casos de teste para a função `productDetails`', () => {
+  it('Verifica se a função `productDetails` tem o comportamento esperado', () => {
+    // fail('Teste vazio!');
+    // ESCREVA SEUS TESTES ABAIXO:
+    // Teste se productDetails é uma função.
+    expect(typeof productDetails).toBe('function');
+    // Teste se o retorno da função é um array.
+    expect(Array.isArray(productDetails('Alcool gel', 'Máscara'))).toBeTruthy();
+    // Teste se o array retornado pela função contém dois itens dentro.
+    expect(productDetails('Ventilador', 'Ar Condicionado')).toHaveLength(2);
+    // Teste se os dois itens dentro do array retornado pela função são objetos.
+    let arrReturn = productDetails('Ventilador', 'Ar Condicionado');
+    for (const obj of arrReturn) {
+      expect(typeof obj).toBe('object');
+      // Teste se os dois productIds terminam com 123.
+      expect(arrReturn[0].details.productId).toMatch(/123/);
+
+    }
+    // Teste se quando passado parâmetros diferentes entre si, os dois objetos também são diferentes entre si.
+    expect(arrReturn[0]).not.toEqual(arrReturn[1]);
+  });
+});
