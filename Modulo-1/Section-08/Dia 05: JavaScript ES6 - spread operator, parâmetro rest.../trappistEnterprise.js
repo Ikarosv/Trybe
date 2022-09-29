0faa41bbafa380617a9799f89001b34810506913
Ikarosv <ikarovieiraa@gmail.com>
2022-09-28 23:24:35 -0300

    Para fixar 2 feito

---
+++ Modulo-1/Section-08/Dia 05: JavaScript ES6 - spread operator, parâmetro rest.../trappistEnterprise.js
@@ -0,0 +1,22 @@
+
+const user = {
+  name: 'Maria',
+  age: 21,
+  nationality: 'Brazilian',
+};
+
+const jobInfos = {
+  profession: 'Software engineer',
+  squad: 'Rocket Landing Logic',
+  squadInitials: 'RLL',
+};
+
+const userInfos = {...user, ...jobInfos};
+
+// console.log(userInfos);
+
+const {name: userN, age, nationality, profession, squadInitials,squad} = userInfos;
+
+console.log(`Hi, my name is ${userN}, I'm ${age} years old and I'm ${nationality}. I work as a ${profession} and my squad is ${squadInitials}-${squad}`);
+
+// Hi, my name is Maria, I'm 21 years old and I'm Brazilian. I work as a Software engineer and my squad is RLL-Rocket Landing Logic
